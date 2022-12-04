839f3743b6f7b18da3d709ded4bef2302ed1fd3e
NPSoftware68 <thanhtra.sqcb@icloud.com>
2022-12-04 14:02:26 +0700

    Initial Commit

---
+++ Rock Paper Scissors/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Rock Paper Scissors
+//
+//  Created by Thanh Tra Le on 04/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rock Paper Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock Paper Scissors
+//
+//  Created by Thanh Tra Le on 04/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
